d282a7c6e4cf9efdb36e1d5b72cc0a4647fd4625
faheem92mt <faheemibnhabib@gmail.com>
2022-04-07 21:47:35 +0800

    First Commit

---
+++ app/src/main/java/company/faheem92mt/Common/NodeNames.java
@@ -0,0 +1,12 @@
+package company.faheem92mt.Common;
+
+public class NodeNames {
+
+    public static final String USERS = "Users";
+
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String ONLINE = "online";
+    public static final String PHOTO = "photo";
+
+}
---
+++ app/src/main/java/company/faheem92mt/login/LoginActivity.java
@@ -0,0 +1,79 @@
+package company.faheem92mt.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import company.faheem92mt.R;
+import company.faheem92mt.signup.SignupActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // mentioning the "TextInputLayout" s for the email and password; this is just mentioning; no relation with the front-end stuff yet
+    private TextInputEditText etEmail, etPassword;
+    // creating 2 empty String variables for the email and password
+    private String email, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // connecting the previously created variables with the actual front-end stuff
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+    }
+
+    public void tvSignupClick(View v) {
+        startActivity(new Intent(this, SignupActivity.class));
+    }
+
+    public void btnLoginClick(View v) {
+        // connecting the "TextInputLayout" s of the front-end with the previously created String variables; only possible after the step on line 31-32
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        // when the email is empty
+        if (email.equals("")) {
+            etEmail.setError(getString(R.string.enter_email));
+        }
+        // when the password is empty
+        else if (password.equals("")) {
+            etPassword.setError(getString(R.string.enter_password));
+        }
+        // when both are not empty
+        else {
+
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    // when the login is successful
+                    if (task.isSuccessful()) {
+
+                    }
+                    // when the login fails
+                    else {
+                        Toast.makeText(LoginActivity.this, "Login Failed : " + task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+        // end of the else condition
+
+
+    }
+}
